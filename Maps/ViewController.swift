d421eda0447630f1879bb815aace24442fc6418c
Anthony Ruiz <yobakaj@gmail.com>
2018-08-10 10:20:19 -0400

    [add] annotations using LongPress and add tittle and subTittle

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -16,17 +16,49 @@ class ViewController: UIViewController, MKMapViewDelegate {
       super.viewDidLoad()
       
       let latitude: CLLocationDegrees = 27.1
+      
       let longitud: CLLocationDegrees = 78.0
+      
       let latDelta: CLLocationDegrees = 0.05
+      
       let lonDelta: CLLocationDegrees = 0.05
+      
       let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
-      let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitud)
-      let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+      
+      let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitud)
+      
+      let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinates, span: span)
       
       map.setRegion(region, animated: true)
       
+      let annotation = MKPointAnnotation()
+      
+      annotation.title = "Taj Mahal"
+      annotation.subtitle = "Lore Lore Lore"
+      annotation.coordinate = coordinates
+      map.addAnnotation(annotation)
+      
+      let lpgr = UILongPressGestureRecognizer(target: self, action: #selector(ViewController.longPress(gesture:)))
+      
+      lpgr.minimumPressDuration = 2
+      map.addGestureRecognizer(lpgr)
+      
       
       // Do any additional setup after loading the view, typically from a nib.
+   }
+   
+   @objc func longPress(gesture: UIGestureRecognizer) {
+      
+      let touchPoint = gesture.location(in: self.map)
+      let coordinate = map.convert(touchPoint, toCoordinateFrom: self.map)
+      
+      let annotacion = MKPointAnnotation()
+      
+      annotacion.coordinate = coordinate
+      annotacion.title = "new tittle"
+      annotacion.subtitle = "Lore Lore"
+      map.addAnnotation(annotacion)
+      
    }
 
    override func didReceiveMemoryWarning() {
