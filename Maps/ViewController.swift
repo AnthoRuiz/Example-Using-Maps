d5dafc695faf0d486fe628805accc41a5bf71031
Anthony Ruiz <yobakaj@gmail.com>
2018-08-10 10:02:01 -0400

    basic example using maps on IOS, Swift 4

--- Maps/ViewController.swift
+++ Maps/ViewController.swift
@@ -7,11 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+   @IBOutlet weak var map: MKMapView!
    override func viewDidLoad() {
       super.viewDidLoad()
+      
+      let latitude: CLLocationDegrees = 27.1
+      let longitud: CLLocationDegrees = 78.0
+      let latDelta: CLLocationDegrees = 0.05
+      let lonDelta: CLLocationDegrees = 0.05
+      let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+      let location: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitud)
+      let region: MKCoordinateRegion = MKCoordinateRegion(center: location, span: span)
+      
+      map.setRegion(region, animated: true)
+      
+      
       // Do any additional setup after loading the view, typically from a nib.
    }
 
